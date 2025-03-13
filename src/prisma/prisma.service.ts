6b3282b6062bb9b8d2ced8248b76cc6bd7a84668
Luiz Chiaradia <luizexchiaradia@gmail.com>
2025-03-13 09:55:26 -0300

    Serviço prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: ['error', 'warn'],
+      errorFormat: 'pretty',
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+
+}
