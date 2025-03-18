044db388c36ec8bf4546800ecf05fb606716ffd8
Luiz Chiaradia <luizexchiaradia@gmail.com>
2025-03-18 09:04:36 -0300

    Configurando autenticação JWT

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 });
 
